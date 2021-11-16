c1173ebcbfe705803ee538946056404647165d95
roxy_GH <rgil@estudiantes.crackthecode.la>
2021-11-16 15:59:58 -0500

    holi

---
+++ grupo65/script.js
@@ -0,0 +1,9 @@
+/*let number = 0;
+while(number <= 5) {
+ console.log(number);
+ number = number + 1;}*/
+ let number = 0;
+ while(number <= 100 ){
+     console.log(number);
+    number = number + 2; 
+}
