9253a519f9efa671f93cd438608dc676f77101dd
roxy_GH <rgil@estudiantes.crackthecode.la>
2021-11-17 16:08:19 -0500

    hola

---
+++ code26/index.js
@@ -0,0 +1,8 @@
+function suma(a, b) {
+    resultado = a+b
+    return resultado;  
+  }
+  
+  let respuesta = suma(1,7);
+  alert(respuesta);
+  
--- grupo65/script.js
+++ grupo65/script.js
@@ -1,9 +1,14 @@
 /*let number = 0;
 while(number <= 5) {
  console.log(number);
- number = number + 1;}*/
+ number = number + 1;}
  let number = 0;
  while(number <= 100 ){
      console.log(number);
     number = number + 2; 
+}*/
+let number = 1;
+ while(number <= 12 ){
+     console.log(number);
+    number = number + 1; 
 }
